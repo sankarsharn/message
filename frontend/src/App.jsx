719b9189523e8d6db37a6f4a62c899269f09af21
sankarsharn <sankarsharnrastogi@gmail.com>
2025-07-15 03:12:00 +0530

    few changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter } from "react-router-dom"
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <h1 class="text-3xl font-bold underline">
       Hello world!
       </h1>
-    </>
+    </BrowserRouter>
   )
 }
 
